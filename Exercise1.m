696eac21beb807b0802eee7ef4cba13067f74394
Billy Joe Andrade <98206217+Mojhayjhay@users.noreply.github.com>
2022-07-29 10:13:57 +0800

    Add files via upload

---
+++ Exercise1.m
@@ -0,0 +1,6 @@
+sampleimg = imread('C:\Users\IT LITERACY TRAINING\Documents\Elective 3\sunflower.jpg');
+imwrite(sampleimg, 'C:\Users\IT LITERACY TRAINING\Documents\Elective 3\sunflower2.jpg');
+grayimg = rgb2gray(sampleimg);
+imwrite(grayimg, 'C:\Users\IT LITERACY TRAINING\Documents\Elective 3\sunflower3.jpg');
+
+
